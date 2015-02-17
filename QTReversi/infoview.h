8fbfc8108eef856ddd32034ace4ec4538dcca3a0
lafargl <renaldo.weiser@gmx.de>
2015-02-17 13:33:47 +0100

    final version

--- QTReversi/infoview.h
+++ QTReversi/infoview.h
@@ -1,5 +1,5 @@
-#ifndef INFOVIEW_H
-#define INFOVIEW_H
+#ifndef _INFOWINDOW_H
+#define _INFOWINDOW_H
 
 #include <QtWidgets>
 #include <define.h>
@@ -13,62 +13,60 @@ class QGridLayout;
 class QTextEdit;
 class QSizePolicy;
 
-class InfoView : public QWidget
+class infoView : public QWidget
 {
-    Q_OBJECT
-public:
-    InfoView(QWidget *parent=0);
-    ~InfoView();
+        Q_OBJECT
 
-    void connecting( game* );
-    void writeSettings();
-    void readSettings();
-    Player* getPlayer(int);
+        public:
+                //Konstruktor
+                infoView(QWidget *parent=0);
+                ~infoView();
+                void connecting( game* );
+                void writeSettings();
+                void readSettings();
+                Player* getPlayer(int);
 
-    void setNameP1( QString name );
-    void setNameP2( QString name );
-    void setTypeP1( TypePlayer );
-    void setTypeP2( TypePlayer );
-    void setScoreP1( int score );
-    void setScoreP2( int score );
-    void setMovesP1( int num );
-    void setMovesP2( int num );
+                void setColorP1( ColorPawn );
+                void setColorP2( ColorPawn );
+                void setNameP1( QString name );
+                void setNameP2( QString name );
+                void setTypeP1( TypePlayer );
+                void setTypeP2( TypePlayer );
+                void setScoreP1( int score );
+                void setScoreP2( int score );
+                void setMovesP1( int num );
+                void setMovesP2( int num );
 
-private:
+        private:
+                Player **playerTable;
+                QLabel *colorP1, *colorP2,
+                        *namepar1, *namepar2,
+                        *levelpar1, *levelpar2,
+                        *scoreP1Label, *scoreP2Label,
+                        *movesP1Label, *movesP2Label,
+                        *nameP1, *nameP2,
+                        *scoreP1, *scoreP2,
+                        *movesP1, *movesP2,
+                        *typeP1, *typeP2,
+                        *infosGame,
+                        *line, *line2,
+                        *dot1, *dot2, *dot3, *dot4, *dot5,
+                        *pawnBlack, *pawnWhite;
 
-    Player **playerTable;
+                QPainter *hit,*hit2;
+                QVBoxLayout *mainLayout;
+                QGridLayout *topLayout;
+                QHBoxLayout *toptopLayout;
 
-    QLabel  *namepar1, *namepar2,
-            *levelpar1, *levelpar2,
-            *nameP1, *nameP2,
-            *typeP1, *typeP2,
-            *line, *line2,
-            *pawnBlack, *pawnWhite,
-            *infosGame,
-            *scoreLabel,
-            *scoreP1, *scoreP2,
-            *movesLabel,
-            *movesP1, *movesP2,
-            *movesListLabel,
-            *dot1, *dot2, *dot3, *dot4, *dot5;
+                void initComponent();
 
-    QPainter *painterBlack,*painterWhite;
-
-    QVBoxLayout *mainLayout;
-    QGridLayout *topLayout;
-    QHBoxLayout *toptopLayout, *centerLayout;
-
-    void initComponent();
-
-public slots:
-
-    void updatingName(Player*);
-    void updatingType(Player*);
-    void setInfosGame(IdMsgInfos);
-    void updatingScore();
-    void updatingMoves();
-
+        public slots:
+                void updatingName(Player*);
+                void updatingType(Player*);
+                void updatingColor(Player*);
+                void updatingScore();
+                void updatingMoves();
+                void setInfosGame(IdMsgInfos);
 };
 
-#endif // INFOVIEW_H
-
+#endif
