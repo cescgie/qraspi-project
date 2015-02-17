9cfd0b60e7788f2d49d84eab1d487e4f7efd28cd
lafargl <renaldo.weiser@gmx.de>
2015-02-17 13:16:22 +0100

    final version

--- QTReversi/board.h
+++ QTReversi/board.h
@@ -1,55 +1,55 @@
-#ifndef BOARD_H
-#define BOARD_H
+#ifndef _BOARD_H
+#define _BOARD_H
 
 #include "define.h"
 #include "square.h"
+//#include <QSoundEffect>
 
 class Board
 {
+    private:
+        //2D-Array, des Brettes
+        square boardTable[8][8];
+        //Brettanimation = mindestens ein Feld in der Animation
+        bool animated;
 
-private:
+    public:
+        //Konstruktor
+        Board();
+        //Initialisiert ein neues Spiel für Reversi
+        void initialization();
+        //setzt ein Stein auf Brett
+        bool addPawn(int,int,ColorPawn);
+        //dreht ein Stein
+        bool turnDownPawn(int,int);
+        //macht ein gueltigen Zug
+        bool addRegularMove(int,int);
+        //entfernt die Animationen
+        void clearAnimation();
+        //BoolVariable 'animiert'
+        void setAnimated(bool);
+        //Zugriffsvariable, der den Zustand der Animation Bord kehrt
+        bool getAnimated();
+        //Zugriffsvariable, gibt die Art eines Kastens zurueck
+        TypeSquare getTypeSquareBoard(int,int);
+        //Zugriffsvariable, gibt die Farbe eines Steines zurueck
+        ColorPawn getColorPawnBoard(int,int);
+        //Zugriffsvariable, gibt den Zustand der Animation zurück
+        bool getAnimatedSquare(int,int);
+        //Zugriffsvariable, liefert den Wert von 'lastMove'
+        bool getLastMoveSquare(int,int);
+        //Leeres Feld (x, y)
+        void emptySquare(int,int);
+        //letzter Zug 'lastMove'(x,y)
+        void enableLastMoveSquare(int,int);
+        //Entfernt gueltige Zuege
+        void clearRegularMove();
 
-    Square boardTable[8][8];
-
-    bool animated;
-
-public:
-    Board();
-
-    void initialization();
-
-    bool addPawn(int,int,ColorPawn);
-
-    bool turnDownPawn(int,int);
-
-    bool addRegularMove(int,int);
-
-    void clearAnimation();
-
-    void setAnimated(bool);
-
-    bool getAnimated();
-
-    TypeSquare getTypeSquareBoard(int,int);
-
-    ColorPawn getColorPawnBoard(int,int);
-
-    bool getAnimatedSquare(int,int);
-
-    bool getLastMoveSquare(int,int);
-
-    void emptySquare(int,int);
-
-    void enableLastMoveSquare(int,int);
-
-    void clearRegularMove();
-
-
-private:
-
-    void emptyBoard();
-
-    void clearLastMove();
+    private:
+        //leert das Brett
+        void emptyBoard();
+        //Entfernt den letzten Zug
+        void clearLastMove();
 };
 
-#endif // BOARD_H
+#endif
