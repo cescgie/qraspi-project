692ff7884c4937e04872170b11be4a222c470b11
lafargl <renaldo.weiser@gmx.de>
2015-02-17 13:34:48 +0100

    final version

--- QTReversi/main.cpp
+++ QTReversi/main.cpp
@@ -1,28 +1,29 @@
-#include "graphics.h"
-#include "game.h"
 #include <QApplication>
 #include <QtWidgets>
+#include "graphics.h"
+#include "game.h"
 #include "ai.h"
+#include "infoview.h"
 
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[])
+int main( int argc, char *argv[])
 {
     srand( time(NULL) );
 
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
 
     game *ge = new game();
-    graphics *grap= new graphics();
-    ai *aipt = new ai();
+    graphics *grap = new graphics();
+    ai *aieng = new ai();
 
     grap->initialization( ge );
-    aipt->connecting(ge);
+    aieng->connecting( ge );
 
     grap->show();
 
-    return a.exec();
+    return app.exec();
 }
