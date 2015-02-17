3b48cf493728dc280d1800349c4a47c60473a767
lafargl <renaldo.weiser@gmx.de>
2015-02-17 13:21:30 +0100

    final version

--- QTReversi/graphics.h
+++ QTReversi/graphics.h
@@ -1,20 +1,21 @@
-#ifndef GRAPHICS_H
-#define GRAPHICS_H
+#ifndef _GRAPHICS_H
+#define _GRAPHICS_H
 
 #include <QMainWindow>
 #include "glview.h"
 #include "infoview.h"
-#include <QtWidgets>
+#include "game.h"
 #include "playersetting.h"
+#include "QSoundEffect"
 #include "listmoves.h"
 #include "highscore.h"
 #include <QtGui>
+#include <QPrinter>
+#include <QPrintDialog>
 #include <QTableView>
 #include <QPixmap>
 #include <QTextDocument>
 #include <QSqlQueryModel>
-#include <QPrinter>
-#include <QPrintDialog>
 
 class game;
 class Player;
@@ -30,108 +31,121 @@ class graphics : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    graphics();
-    ~graphics();
-    void initialization( game * );
+    public:
+        graphics();
+        ~graphics();
+        void initialization( game * );
 
-    QSqlQueryModel* getHighscores();
-protected:
+        QSqlQueryModel* getHighscores();
+    protected:
         void closeEvent(QCloseEvent *event);
 
-private slots:
-    void newGame();
-    void preferences();
-    void displayWinner(Player*);
-    void updateSettingAnimation();
-    void updateSettingRegularMoves();
-    void updateSettingLastMove();
-    void undoMove();
-    void redoMove();
-    void setDisplayUndoMoveAction(bool);
-    void setDisplayRedoMoveAction(bool);
-    void fullScreen();
-    void showGlNormal();
+    private slots:
+        void newGame();
+        bool saveGame();
+        bool saveFile();
+        void loadGame();
+        bool maybeSave();
+        void choose_pic();
+        void preferences();
+        void undoMove();
+        void redoMove();
+        void fullScreen();
+        void showGlNormal();
+        void displayWinner(Player*);
+        void updateSettingRegularMoves();
+        void updateSettingLastMove();
+        void updateSettingAnimation();
+        void updateSettingVolume();
+        void setDisplayUndoMoveAction(bool);
+        void setDisplayRedoMoveAction(bool);
+        void onButtonHelpPressed();
+        void onButtonAboutPressed();
+        void refreshHighScore();
+        void cleanHighScore();
+        void printHighScore();
+        void deleteHighscores();
 
-    void refreshHighScore();
-    void printHighScore();
-    void cleanHighScore();
-    void deleteHighscores();
+    private:
+        void initComponent();
+        void HighscoreView();
+        void soundsConfiguration();
+        void createActions();
+        void createMenus();
+        void createToolBars();
+        void createStatusBar();
+        void connecting( game * );
+        QDialog *helpDialog;
+        QDialog *aboutDialog;
+        void writeSettings();
+        void readSettings();
+        QWidget *centralWindow;
+        glView *scene;
+        infoView *infos;
+        game *savlo;
+        ListMoves *list_moves;
+        QLabel *statusBarLabel;
+        QMenu *fileMenu;
+        QMenu *viewMenu;
+        QMenu *toolsMenu;
+        QMenu *helpMenu;
+        QToolBar *fileToolBar;
+        QToolBar *toolsToolBar;
 
-    void onButtonHelpPressed();
-    void onButtonAboutPressed();
-private:
-     void initComponent();
-     void createActions();
-     void createMenus();
-     void createStatusBar();
-     void createToolBars();
-     void soundConfiguration();
+//Aktionsmenues "Datei"
+        QAction *newAction;
+        QAction *saveAction;
+        QAction *loadAction;
+        QAction *exitAction;
 
-     void connecting( game * );
+//Aktionsmenues "Ansicht"
+        QAction *regularMovesAction;
+        QAction *lastMoveAction;
+        QAction *animationAction;
+        QAction *volumeAction;
 
-     QWidget *centralWindow;
-     glView *scene;
-     InfoView *infos;
-     ListMoves *list_moves;
+//Aktionsmenues "Werkzeuge"
+        QAction *undoMoveAction;
+        QAction *redoMoveAction;
+        QAction *preferencesAction;
+        QAction *loadBoard;
+        QAction *fullScreenAction;
 
-     QLabel *statusBarLabel;
+//Aktionsmenues "Hilfe"
+        QAction *aboutAction;
+        QAction *helpAction;
+        //board background
+        QPixmap image;
+        QImage  *imageObject;
+        //sound
+        QSoundEffect effectStart;
+        QSoundEffect effectEnd;
+        QSoundEffect effectBounce;
+        QSoundEffect effectPrev;
+        QSoundEffect effectNext;
+        Highscore _highscore;
+        QPushButton *buttonPrint;
+        QPushButton *buttonRefreshHS;
+        QPushButton *buttonClearHS;
+        QVBoxLayout *verticalLayout;
+        QVBoxLayout *verticallayout2;
+        QVBoxLayout *topverticalLayout;
+        QHBoxLayout *horizontalLayout;
+        QHBoxLayout *buttonLayout;
+        QLabel *highscoresLabel;
+        QAction *printAction;
+        QAction *RefreshHSAction;
+        QAction *ClearHSAction;
+        QAction *HSAction;
+        QGridLayout *gridLayout;
+        QTableView *tableViewHighscores;
+        QGroupBox *groupBoxHighscores;
+        void createHighscore(QString, int);
 
-     QMenu *fileMenu;
-     QMenu *viewMenu;
-     QMenu *toolsMenu;
-
-     QToolBar *fileToolBar;
-     QToolBar *toolsToolBar;
-
-     QAction *newAction;
-     QAction *exitAction;
-
-     QAction *animationAction;
-     QAction *preferencesAction;
-     QAction *regularMovesAction;
-     QAction *lastMoveAction;
-     QAction *undoMoveAction;
-     QAction *redoMoveAction;
-
-     //sound
-     QSoundEffect effectStart;
-     QSoundEffect effectEnd;
-     QSoundEffect effectBounce;
-     QSoundEffect effectPrev;
-     QSoundEffect effectNext;
-
-     QAction *fullScreenAction;
-
-     void HighscoreView();
-
-     Highscore _highscore;
-     QVBoxLayout *verticalLayout;
-     QVBoxLayout *verticallayout2;
-     QVBoxLayout *topverticalLayout;
-     QHBoxLayout *horizontalLayout;
-     QLabel *highscoresLabel;
-     QAction *printAction;
-     QAction *RefreshHSAction;
-     QAction *ClearHSAction;
-     QAction *HSAction;
-     QPushButton *buttonPrint;
-     QHBoxLayout *buttonLayout;
-
-     QGridLayout *gridLayout;
-     QTableView *tableViewHighscores;
-     QGroupBox *groupBoxHighscores;
-
-     void createHighscore(QString, int);
-
-     QDialog *helpDialog;
-     QAction *helpAction;
-     QDialog *aboutDialog;
-     QAction *aboutAction;
-signals:
-    void startNewGame();
-    void undoMoveAsked();
-    void redoMoveAsked();
+        signals:
+                void startNewGame();
+                void undoMoveAsked();
+                void redoMoveAsked();
 };
 
-#endif // GRAPHICS_H
+#endif
