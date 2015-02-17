1ead28808facd72415e5eaf729e05135afa25032
lafargl <renaldo.weiser@gmx.de>
2015-02-17 13:41:34 +0100

    final version

--- QTReversi/square.cpp
+++ QTReversi/square.cpp
@@ -1,7 +1,7 @@
 #include "square.h"
 
 //Konstruktor: initialisiert alle Variablen
-Square::Square()
+square::square()
 {
     type = Empty;
     color = White;
@@ -10,66 +10,57 @@ Square::Square()
 }
 
 
-//Wechsel der Variable 'Typ' und 'Farbe'
-void Square::setSquare(TypeSquare type, ColorPawn color)
+//Wechsel 'Typ' und 'Farbe'
+void square::setSquare(TypeSquare type, ColorPawn color)
 {
     this->type = type;
     this->color = color;
 }
 
-
-//Wechsel der Variable 'Typ'
-void Square::setType(TypeSquare type)
+//Wechsel 'Typ'
+void square::setType(TypeSquare type)
 {
     this->type = type;
 }
 
-
-//Wechsel der Variable 'Farbe'
-void Square::setColor(ColorPawn color)
+//Wechsel 'Farbe'
+void square::setColor(ColorPawn color)
 {
     this->color = color;
 }
 
-
-//Wechsel der Variable 'animiert'
-void Square::setAnimated(bool b)
+//Wechsel 'animiert'
+void square::setAnimated(bool b)
 {
     animated = b;
 }
 
-
-//Wechsel der Variable 'Letzter Zug'
-void Square::setLastMove(bool b)
+//Wechsel 'Letzter Zug'
+void square::setLastMove(bool b)
 {
     lastMove = b;
 }
 
-
 //Zugriffsvariable 'Typ'
-TypeSquare Square::getType()
+TypeSquare square::getType()
 {
     return type;
 }
 
-
 //Zugriffsvariable 'Farbe'
-ColorPawn Square::getColor()
+ColorPawn square::getColor()
 {
     return color;
 }
 
-
 //Zugriffsvariable 'animiert'
-bool Square::getAnimated()
+bool square::getAnimated()
 {
     return animated;
 }
 
-
 //Zugriffsvariable 'Letzter Zug'
-bool Square::getLastMove()
+bool square::getLastMove()
 {
     return lastMove;
 }
-
