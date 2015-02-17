0df3c4fc2eff3e90958c523ebe3ec7651693548b
lafargl <renaldo.weiser@gmx.de>
2015-02-17 13:32:20 +0100

    final version

--- QTReversi/infosmoves.h
+++ QTReversi/infosmoves.h
@@ -2,58 +2,43 @@
 #define _INFOSMOVES_
 
 #include <vector>
-#include "moves.h"
-
-class InfosMoves
+#include "imove.h"
+class imove;
+class infosMoves
 {
         public:
-
                 //Konstruktor
-                InfosMoves();
-//              infosMoves(int,vector<moves>,vector<moves>);
-
+                infosMoves();
 
         private:
-
                 //Anzahl der Steine in Liste
-                vector<moves> pawnsTurnedDownList;
-
+                vector<imove> pawnsTurnedDownList;
                 //Anzahl der gueltigen Bewegungen
-                vector<moves> regularMovesList;
-
+                vector<imove> regularMovesList;
                 //aktuelle Spieler ID
                 int currentPlayer;
 
-
         public:
-
-                //Setzen Sie eines Steins in die Liste
-                void addPawnsTurnedDown(moves);
-
+                //setzt ein Stein in die Liste
+                void addPawnsTurnedDown(imove);
                 //sichern der gueltigen Bewegungen
-                void saveRegularMoves(vector<moves>);
-
+                void saveRegularMoves(vector<imove>);
                 //sichern der aktuellen Spieler
                 void saveCurrentPlayer(int);
-
                 //Zugriffsvariable 'currentPlayer'
                 int getCurrentPlayer();
-
                 //Zugriffsvariable 'pawnsTurnedDownList'
-                vector<moves> getPawnsTurnedDownList();
-
+                vector<imove> getPawnsTurnedDownList();
                 //Zugriffsvariable 'regularMovesList'
-                vector<moves> getRegularMovesList();
-
+                vector<imove> getRegularMovesList();
                 //gibt die Anzahl von 'pawnsTurnedDownList'
                 int getSizePawnsTurnedDownList();
                 //gibt die Anzahl von 'regularMovesList'
                 int getSizeRegularMovesList();
                 //Gibt die Anzahl der Zuege 'regularMovesList'
-                moves getRegularMovesList(int);
+                imove getRegularMovesList(int);
                 //Gibt die Anzahl der Zuege 'pawnsTurnedDownList'
-                moves getPawnsTurnedDownList(int);
-
+                imove getPawnsTurnedDownList(int);
                 //Anzeige im Fenster
                 void affichage();
 };
